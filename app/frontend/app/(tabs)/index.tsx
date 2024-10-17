343df49769d25aa6c0b8c2d554291b49b7fa53e5
Zeleo21 <leo.guazzaroni@epita.fr>
2024-10-17 10:57:58 +0200

    feat: added nativewind -> tailwindCSS for react-native to the project

--- app/frontend/app/(tabs)/index.tsx
+++ app/frontend/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {Image, StyleSheet, Platform, View} from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -6,7 +6,7 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
-  return (
+  return(
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
@@ -46,7 +46,7 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
-    </ParallaxScrollView>
+   </ParallaxScrollView>
   );
 }
 
---
+++ app/frontend/app/test.tsx
@@ -0,0 +1,14 @@
+import {View} from "react-native";
+
+
+const Test = () => {
+    return (
+        <div>
+           <View className="max-h-none">
+
+           </View>
+            <h1>Test</h1>
+        </div>
+    )
+}
+export default Test;
