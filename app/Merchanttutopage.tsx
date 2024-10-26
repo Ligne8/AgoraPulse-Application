7ffb669d10e136de452098f320612c9e2aa6743c
tom.ami <tom.ami@epita.fr>
2024-10-27 00:09:05 +0200

    LIG-163: not working

---
+++ app/Merchanttutopage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Merchanttutopage() {
+  return (
+    <View>
+      <Text>aaeae</Text>
+    </View>
+  );
+}
