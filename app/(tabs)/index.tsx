7e0113ea3619567a76dabb94968b2c3f49eb6620
Ronan <ronan.pedron@epita.fr>
2024-10-29 10:59:22 +0100

    fix: CamelCase for WelcomePage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { useLayoutEffect, useEffect } from "react";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import CustomButton from "@/components/CustomButton"; // Importe ton composant bouton
-import WelcomeScreen from "@/app/(tabs)/welcompage";
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useLayoutEffect, useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import CustomButton from '@/components/CustomButton'; // Importe ton composant bouton
+import WelcomePage from '@/app/(tabs)/WelcomPage';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function index() {
-  return <>{WelcomeScreen}</>;
+  return <>{WelcomePage}</>;
 }
