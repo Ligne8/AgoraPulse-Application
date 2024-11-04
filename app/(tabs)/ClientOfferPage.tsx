5ba27089429851bc12889a06881ff0880141f8d5
Ronan <ronan.pedron@epita.fr>
2024-11-04 15:07:07 +0100

    fix: lintter

--- app/(tabs)/ClientOfferPage.tsx
+++ app/(tabs)/ClientOfferPage.tsx
@@ -1,6 +1,6 @@
-import { Text, Touchable, TouchableOpacity, View, Image, ScrollView } from "react-native";
-import React from "react";
-import { FontAwesome } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View, Image, ScrollView } from 'react-native';
+import React from 'react';
+import { FontAwesome } from '@expo/vector-icons';
 
 interface Offer {
   title: string;
@@ -8,7 +8,7 @@ interface Offer {
   code: string;
 }
 
-function Offer({ title, picture_url, code }: Offer) {
+function Offer({ title, picture_url }: Offer) {
   return (
     <View className="mb-[10px]">
       <View className="justify-around items-center flex-row w-[332px] h-[120px] border-[1px] border-[#CCCCCC] bg-[#EEEEEE] rounded-t-[10px]">
@@ -26,26 +26,26 @@ function Offer({ title, picture_url, code }: Offer) {
 }
 
 export default function ClientOfferPage() {
-  const [offers, setOffers] = React.useState<Offer[]>([
+  const [offers] = React.useState<Offer[]>([
     {
-      title: "Profitez de -10% sur votre prochain pack de 24 canettes.",
-      picture_url: "https://images.affiches-et-posters.com//albums/3/55722/medium/affiche-vintage-perrier-villemot-7411.jpg",
-      code: "CODE1",
+      title: 'Profitez de -10% sur votre prochain pack de 24 canettes.',
+      picture_url: 'https://images.affiches-et-posters.com//albums/3/55722/medium/affiche-vintage-perrier-villemot-7411.jpg',
+      code: 'CODE1',
     },
     {
-      title: "Profitez de -25% sur votre prochain achat.",
-      picture_url: "https://www.toutelasignaletique.com/23574-large_default/plaque-publicite-vache-qui-rit.jpg",
-      code: "CODE1",
+      title: 'Profitez de -25% sur votre prochain achat.',
+      picture_url: 'https://www.toutelasignaletique.com/23574-large_default/plaque-publicite-vache-qui-rit.jpg',
+      code: 'CODE1',
     },
     {
-      title: "Profitez de -25% sur votre prochain achat.",
-      picture_url: "https://www.toutelasignaletique.com/23574-large_default/plaque-publicite-vache-qui-rit.jpg",
-      code: "CODE1",
+      title: 'Profitez de -25% sur votre prochain achat.',
+      picture_url: 'https://www.toutelasignaletique.com/23574-large_default/plaque-publicite-vache-qui-rit.jpg',
+      code: 'CODE1',
     },
     {
-      title: "Profitez de -25% sur votre prochain achat.",
-      picture_url: "https://www.toutelasignaletique.com/23574-large_default/plaque-publicite-vache-qui-rit.jpg",
-      code: "CODE1",
+      title: 'Profitez de -25% sur votre prochain achat.',
+      picture_url: 'https://www.toutelasignaletique.com/23574-large_default/plaque-publicite-vache-qui-rit.jpg',
+      code: 'CODE1',
     },
   ]);
 
@@ -53,7 +53,7 @@ export default function ClientOfferPage() {
     <View className="h-full flex-col w-full justify-center items-center">
       <View className="justify-center items-center px-[30px] mb-[20px] mt-10">
         <Text className="text-[36px] font-bold text-[#0E3D60] mb-6">Vos offres exclusives</Text>
-        <Text className="text-center text-[#0E3D60] text-[16px]">Découvrez les promotions des commerces que vous avez rencontrés aujourd'hui. Profitez-en avant qu’elles ne disparaissent !</Text>
+        <Text className="text-center text-[#0E3D60] text-[16px]">Découvrez les promotions des commerces que vous avez rencontrés aujourd&apos;hui. Profitez-en avant qu&apos;elles ne disparaissent !</Text>
       </View>
       <View className=" h-[450px]">
         <ScrollView className=" " showsVerticalScrollIndicator={false}>
