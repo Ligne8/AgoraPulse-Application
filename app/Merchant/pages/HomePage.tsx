07202a6747704e3c58d5bba849b6ea44d010e5c7
tom.ami <tom.ami@epita.fr>
2024-11-14 16:39:57 +0100

    LIG-167: dev merchant home page

---
+++ app/Merchant/pages/HomePage.tsx
@@ -0,0 +1,39 @@
+import { useFonts } from 'expo-font';
+import { SplashScreen } from 'expo-router';
+import React, { useEffect } from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
+
+export default function HomePage() {
+  const [fontsLoaded] = useFonts({
+    Montserrat: require('@/assets/fonts/Montserrat-Regular.ttf'),
+    MontserratBold: require('@/assets/fonts/Montserrat-Bold.ttf'),
+    MontserratExtraBolt: require('@/assets/fonts/Montserrat-ExtraBold.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  return (
+    <View className="w-full h-full mt-14">
+      <View className="justify-center items-center">
+        <Image className="w-[125px] h-[125px] mb-[25px]" source={require('@/assets/images/logo.png')} />
+        <Text style={styles.text} className="text-center text-[36px] font-bold text-[#0E3D60] mb-6">
+          Votre espace commerçant
+        </Text>
+        <Text style={styles.text} className="text-center text-[16px] text-[#0E3D60] px-6">
+          Gérez vos annonces et vos offres de fidélité pour engager votre clientèle de proximité.
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'Montserrat',
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import WelcomePage from './WelcomePage';
+import HomePage from './Merchant/pages/HomePage';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function index() {
-  return <WelcomePage />;
+  return <HomePage />;
 }
