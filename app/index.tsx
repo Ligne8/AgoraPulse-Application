fb31ed23ede562067f5c642808cf697a0f4aa4fc
timothee.drode <timothee.drode@epita.fr>
2024-11-18 20:14:04 +0100

    index good linter

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import WelcomePage from './WelcomePage';
-import HomePage from './Merchant/pages/HomePage';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function index() {
-  return <HomePage />;
+    return <WelcomePage/>;
 }
