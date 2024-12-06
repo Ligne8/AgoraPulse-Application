5a187bea9d693d0f58780c21a9b67be97e9eadc6
Ronan <ronan.pedron@epita.fr>
2024-12-06 15:01:49 +0100

    feat: navbar done

--- app/client/(tabs)/_layout.tsx
+++ app/client/(tabs)/_layout.tsx
@@ -1,42 +1,89 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
-import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+import { Entypo, FontAwesome, MaterialIcons } from '@expo/vector-icons';
+import { StyleSheet } from 'react-native';
 
 export default function ClientTabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarStyle: styles.tabBar,
+        tabBarActiveTintColor: '#0E3D60',
+        tabBarInactiveTintColor: '#8E8E93',
+      }}
+    >
       <Tabs.Screen
         name="ClientHome"
         options={{
-          title: 'Accueil',
           headerShown: false,
-          tabBarIcon: ({ color, size }) => <AntDesign name="home" size={size} color={color} />,
+          title: 'Accueil',
+          tabBarIcon: ({ focused, color, size }) => (
+            <Entypo
+              name="home"
+              size={size}
+              color={color}
+              style={focused ? styles.activeIcon : null} // Highlight si actif
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="OfferPage"
         options={{
-          title: 'Offres',
           headerShown: false,
-          tabBarIcon: ({ color, size }) => <MaterialIcons name="discount" size={size} color={color} />,
+          title: 'Offres',
+          tabBarIcon: ({ focused, color, size }) => (
+            <MaterialIcons
+              name="discount"
+              size={size}
+              color={color}
+              style={focused ? styles.activeIcon : null} // Highlight si actif
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="ClientLoyaltyPage"
         options={{
-          title: 'Fidélité',
           headerShown: false,
-          tabBarIcon: ({ color, size }) => <MaterialIcons name="star" size={size} color={color} />,
+          title: 'Fidélité',
+          tabBarIcon: ({ focused, color, size }) => (
+            <MaterialIcons
+              name="star"
+              size={size}
+              color={color}
+              style={focused ? styles.activeIcon : null} // Highlight si actif
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="ProfilePage"
         options={{
-          title: 'Profil',
           headerShown: false,
-          tabBarIcon: ({ color, size }) => <AntDesign name="user" size={size} color={color} />,
+          title: 'Profil',
+          tabBarIcon: ({ focused, color, size }) => (
+            <FontAwesome
+              name="user"
+              size={size}
+              color={color}
+              style={focused ? styles.activeIcon : null} // Highlight si actif
+            />
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderTopColor: '#e5e5e5',
+    height: 75,
+  },
+  activeIcon: {
+    transform: [{ scale: 1.2 }],
+  },
+});
