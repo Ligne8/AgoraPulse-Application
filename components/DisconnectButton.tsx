b9b1afc6cbc7724b7a541820d9d2140acd4c7b64
timothee.drode <timothee.drode@epita.fr>
2025-01-30 11:49:08 +0100

    feat final demo + linter

--- components/DisconnectButton.tsx
+++ components/DisconnectButton.tsx
@@ -1,28 +1,28 @@
-import {Text, TouchableOpacity} from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import supabase from '@/backend/supabase';
-import {useRouter} from 'expo-router';
+import { useRouter } from 'expo-router';
 
 const DisconnectButton = () => {
-    const navigate = useRouter();
-    const handleDisconnection = async () => {
-        const {error} = await supabase.auth.signOut();
-        if (error) {
-            console.error('Error logging out:', error.message);
-            return;
-        }
-        navigate.push('/LoginPage');
-    };
+  const navigate = useRouter();
+  const handleDisconnection = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error('Error logging out:', error.message);
+      return;
+    }
+    navigate.push('/LoginPage');
+  };
 
-    return (
-        <TouchableOpacity
-            className="flex-row rounded-xl border border-[#E74C3C] items-center py-2 px-[84] mb-5"
-            onPress={handleDisconnection}
-            testID="logoutButton"
-        >
-            <Text className=" text-[#E74C3C] font-montserrat">Déconnexion</Text>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity
+      className="flex-row rounded-xl border border-[#E74C3C] items-center py-2 px-[84] mb-5"
+      onPress={handleDisconnection}
+      testID="logoutButton"
+    >
+      <Text className=" text-[#E74C3C] font-montserrat">Déconnexion</Text>
+    </TouchableOpacity>
+  );
 };
 
 export default DisconnectButton;
--- components/ModalButton.tsx
+++ components/ModalButton.tsx
@@ -1,40 +1,44 @@
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 
 export const ModalButton = ({
-                                title,
-                                onPress,
-                                backgroundColor,
-                                textColor,
-                                borderColor = '#FFFFFF00',
-                                testID = '',
-                            }: {
-    title: string;
-    onPress: () => void;
-    backgroundColor: string;
-    textColor: string;
-    borderColor?: string;
-    testID?: string;
+  title,
+  onPress,
+  backgroundColor,
+  textColor,
+  borderColor = '#FFFFFF00',
+  testID = '',
+}: {
+  title: string;
+  onPress: () => void;
+  backgroundColor: string;
+  textColor: string;
+  borderColor?: string;
+  testID?: string;
 }) => (
-    <TouchableOpacity style={[styles.modalButton, {backgroundColor}, {borderColor}]} onPress={onPress} testID={testID}>
-        <Text style={[styles.modalButtonText, {color: textColor}]}>{title}</Text>
-    </TouchableOpacity>
+  <TouchableOpacity
+    style={[styles.modalButton, { backgroundColor }, { borderColor }]}
+    onPress={onPress}
+    testID={testID}
+  >
+    <Text style={[styles.modalButtonText, { color: textColor }]}>{title}</Text>
+  </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
-    modalButton: {
-        textAlign: 'center',
-        justifyContent: 'center',
-        marginHorizontal: 5,
-        marginBottom: 10,
-        borderRadius: 100,
-        height: 50,
-        borderWidth: 1,
-    },
-    modalButtonText: {
-        fontSize: 14,
-        fontFamily: 'MontserratBold',
-        color: '#0E3D60',
-        textAlign: 'center',
-    },
+  modalButton: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 5,
+    marginBottom: 10,
+    borderRadius: 100,
+    height: 50,
+    borderWidth: 1,
+  },
+  modalButtonText: {
+    fontSize: 14,
+    fontFamily: 'MontserratBold',
+    color: '#0E3D60',
+    textAlign: 'center',
+  },
 });
